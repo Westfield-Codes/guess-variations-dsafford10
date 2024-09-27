5f939eebc06c4830ddced5be7d2a7f0c31e0d04e
dsafford10 <dylan209274@student-schoolsofwestfield.org>
2024-09-27 12:09:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,34 +2,32 @@
                                                                                                                                                                                                              
 
 // create global variable guess, set to 0
-
+var guess =0;
 // create a global variable called answer and store a random integer 1-100
-
+var answer = Math.floor(Math.random()*100)+1;
 // define function guessGame
 function guessGame(){
-
 // display the answer for testing purposes only
-  let again = "true"
-
+alert(answer);
 // set guess equal to prompt asking user to "Guess a number, 1-100"
-  
-
+ guess = prompt("Guess a number, 1-100");
 // if guess equals answer display "Correct!" 
-
+alert("Correct");
 // or if guess is greater than answer create a code block
-
-// display "Too high!" 
-
+else if (guess > answer) {
+ // display "Too high!" 
+alert ("Too High!");
 // call guessGame
-
+GuessGame()
 // close the else code block
-
+}
 // otherwise open a code block
-
+else {        
 // display "Too low!" 
-
+alert ("Too Low!");
 // call guessGame
-
+GuessGame()
 // close the else code block
-
+}
 // end the function
+}
