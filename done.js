f43e3ddfbc4fc5e5b27350ddb2761eb22e19359c
dsafford10 <dylan209274@student-schoolsofwestfield.org>
2024-09-27 12:09:43 -0400

    Update done.js

--- done.js
+++ done.js
@@ -1,1 +1,33 @@
-/* Put Done Code Here *.
+/* Simple Guess Program */
+                                                                                                                                                                                                             
+
+// create global variable guess, set to 0
+var guess =0;
+// create a global variable called answer and store a random integer 1-100
+var answer = Math.floor(Math.random()*100)+1;
+// define function guessGame
+function guessGame(){
+// display the answer for testing purposes only
+alert(answer);
+// set guess equal to prompt asking user to "Guess a number, 1-100"
+ guess = prompt("Guess a number, 1-100");
+// if guess equals answer display "Correct!" 
+alert("Correct");
+// or if guess is greater than answer create a code block
+else if (guess > answer) {
+ // display "Too high!" 
+alert ("Too High!");
+// call guessGame
+GuessGame()
+// close the else code block
+}
+// otherwise open a code block
+else {        
+// display "Too low!" 
+alert ("Too Low!");
+// call guessGame
+GuessGame()
+// close the else code block
+}
+// end the function
+}/* Put Done Code Here *.
